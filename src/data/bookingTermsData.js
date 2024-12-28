a7cb4df042963b097bfc20d2722f215b773c531e
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2024-12-28 20:58:34 +0100

    Uppdaterad text

--- src/data/bookingTermsData.js
+++ src/data/bookingTermsData.js
@@ -2,37 +2,37 @@ export const bookingTermsData = [
   {
     title: "Bokning & Betalning",
     details:
-      "Godkänd offert räknas som bokningsbekräftelse. Vid förfrågan preliminärbokas datumet. Totalbeloppet faktureras med 30 dagar kredittid och skickas till kund efter genomfört event.",
+      "En godkänd offert fungerar som bokningsbekräftelse. Vid förfrågan preliminärbokas datumet. Totalbeloppet faktureras med 30 dagars kredittid och skickas till kunden efter genomfört event.",
   },
   {
     title: "Avbokning",
     details:
-      "Bröllopskunder har rätt att omboka till nytt datum ifall nöden kräver, utan extra kostnad. Alla bokningar som avbokas av kund närmare än 7 dagar faktureras fullt belopp. Om inte annat är överrenskommet vid bokningstillfälle.",
+      "Bröllopskunder har rätt att omboka till ett nytt datum vid oförutsedda händelser, utan extra kostnad. Avbokningar som sker mindre än 7 dagar före eventet faktureras fullt belopp, om inte annat har överenskommits vid bokningstillfället.",
   },
   {
     title: "Utrustning & Setup",
     details:
-      "All professionell DJ-utrustning ingår i priset om inte annat är kommunicerat. Setup sker normalt 1-2 timmar innan start, exakt tid bestäms vid konsultation. Kan man använda rökmaskin ingår det utan extra kostnad. Högtalarna av märket Yamaha låter magiskt bra och lämpar sig för upp till 120 personer. För större sällskap tillkommer offert för hyra av kompletterande ljud utrustning och annan utrustning utefter eventets specifika behov.",
+      "All professionell DJ-utrustning ingår i priset om inget annat har meddelats. Setup sker normalt 1-2 timmar innan eventstart, och exakt tid bestäms vid konsultation. Rökmaskin kan användas utan extra kostnad, om det är tillåtet i lokalen. Högtalarna av märket Yamaha är av hög kvalitet och passar för upp till 120 personer. Vid större sällskap erbjuds en offert för kompletterande ljud- och ljusutrustning baserat på eventets specifika behov.",
   },
   {
     title: "Hyra av ljudutrustning",
     details:
-      "För tjänster där ljudutrustning inte ingår som en del av paketet, tillkommer en offert som baseras på ert specifika behov. Ev milersättning tillkommer om ljud ska levereras separat och inte del av något paket.",
+      "För tjänster där ljudutrustning inte ingår som en del av paketet, tillkommer en offert baserad på era specifika behov. Milersättning tillkommer om ljudutrustningen ska levereras separat och inte är en del av något paket.",
   },
   {
     title: "Speltid",
     details:
-      "sebbedj spelar så länge det är partystämning och hålligång på eventet upp till 5 timmar. Vid nattklubbsspelningar, regleras speltid enligt överrenskommelse. För alla andra event tjänster där spelningen går över 5 timmar speltid tillkommer en kostnad på 3000 SEK inkl. moms.",
+      "DJn spelar upp till 5 timmar, så länge partystämningen håller i sig. Vid nattklubbsspelningar regleras speltiden enligt överenskommelse. För spelningar som överskrider 5 timmar tillkommer en kostnad på 3 000 SEK inkl. moms.",
   },
   {
     title: "Sjukdom eller familjär incident",
     details:
-      "Om något så tråkigt som sjukdom eller något svårartat händer försöker vi i första hand ordna med en ersättare om DJn inte kan genomföra eventet. I fall där det inte finns ersättare samt att man av kritiska skäl inte kan genomföra eventet har DJ rätt att avboka. Ingen ersättning utbetalas för utebliven DJ.",
+      "Vid sjukdom eller andra oförutsedda händelser görs ett försök att ordna en ersättare om DJn inte kan genomföra eventet. Om detta inte är möjligt och DJn av kritiska skäl inte kan närvara, har DJn rätt att avboka utan ersättningsskyldighet.",
   },
   {
     title: "Resor, logi och mat",
     details:
-      "Vid resor tillkommer en milersättning på 25 SEK/mil exkl moms. För event längre än 10 mil tillkommer ersättning för övernattning i anslutning till spelningen. Vid event där DJn förvänta anlända och installera innan eventstart erbjuds DJn kostnadsfri middag på plats.",
+      "Milersättning: 25 SEK/mil exkl. moms. Vid resor längre än 10 mil tillkommer ersättning för övernattning i anslutning till spelningen. Om DJn förväntas anlända och installera innan eventstart ska kostnadsfri middag erbjudas på plats.",
   },
   { title: "", details: "Uppdaterad december 2024" },
 ];
